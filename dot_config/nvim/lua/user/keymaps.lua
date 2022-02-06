3420020765f33f6d067784871fe025ec2ff54383
Guilhem Charles <guilhem.charles@gmail.com>
2022-02-06 15:37:07 +0100

    neovim: add keymaps for resizing splits and navigating buffers

--- dot_config/nvim/lua/user/keymaps.lua
+++ dot_config/nvim/lua/user/keymaps.lua
@@ -12,6 +12,17 @@ utils.nnoremap("<C-j>", "<C-w>j")
 utils.nnoremap("<C-k>", "<C-w>k")
 utils.nnoremap("<C-l>", "<C-w>l")
 
+-- Resize splits
+utils.nnoremap("<C-S-left>", "<Cmd>vertical resize -2<CR>")
+utils.nnoremap("<C-S-down>", "<Cmd>resize -2<CR>")
+utils.nnoremap("<C-S-up>", "<Cmd>resize +2<CR>")
+utils.nnoremap("<C-S-right>", "<Cmd>vertical resize +2<CR>")
+
+-- Buffer navigation
+utils.nmap("<A-left>", "<Cmd>bprev<CR>")
+utils.nmap("<A-right>", "<Cmd>bnext<CR>")
+utils.nmap("<leader>e", "<Cmd>Buffers<CR>")  -- list buffers
+
 -- Save with Ctrl + s
 utils.nmap("<C-s>", "<Cmd>w<CR>")
 
