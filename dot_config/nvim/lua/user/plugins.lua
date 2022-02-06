c668388875b8f3d872822b95ad07e044aca825e5
Guilhem Charles <guilhem.charles@gmail.com>
2022-02-06 20:37:40 +0100

    neovim: fix fzf plugins order

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -48,15 +48,27 @@ local plugins = {
     
     -- Navigation
     "justinmk/vim-sneak",
-    "junegunn/fzf.vim",
     {
         "junegunn/fzf",
         run = function()
             vim.fn['fzf#install']()
-        end
+        end,
+        alias = "fzf",
     },
+    {"junegunn/fzf.vim", after = "fzf"},
     "preservim/nerdtree",
-    
+
+    -- Completion
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+
+    -- Snippets
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
+    "saadparwaiz1/cmp_luasnip",
+
     -- Languages
     "rust-lang/rust.vim",
     "cespare/vim-toml",
