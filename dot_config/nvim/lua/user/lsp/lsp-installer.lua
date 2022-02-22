fe7b686a400dedd3f4b0cea8fea9f39087f38fd3
Guilhem Charles <guilhem.charles@gmail.com>
2022-02-22 21:40:35 +0100

    neovim: setup json lsp

--- dot_config/nvim/lua/user/lsp/lsp-installer.lua
+++ dot_config/nvim/lua/user/lsp/lsp-installer.lua
@@ -18,6 +18,11 @@ lsp_installer.on_server_ready(function(server)
         opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
     end
 
+    if server.name == "jsonls" then
+        local jsonls_opts = require("user.lsp.settings.jsonls")
+        opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+    end
+
     -- This setup() function is exactly the same as lspconfig's setup function.
     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     server:setup(opts)
