e6e2e7ff4563caf16b7b92246fb70a9d16b64397
Guilhem C <guilhem.charles@gmail.com>
2022-10-03 19:53:49 +0200

    nvim: remove auto-close behavior for nvim-tree

--- dot_config/nvim/lua/user/nvim-tree.lua
+++ dot_config/nvim/lua/user/nvim-tree.lua
@@ -118,13 +118,3 @@ nvim_tree.setup {
   },
 }
 
--- see https://github.com/kyazdani42/nvim-tree.lua/discussions/1115
-vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-      vim.cmd "quit"
-    end
-  end
-})
-
