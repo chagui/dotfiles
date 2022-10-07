f5bd9b34dac8a44b702df5b7ae7a152778859072
Guilhem C <guilhem.charles@gmail.com>
2022-10-07 19:11:49 +0200

    nvim: replace lightline with lualine

---
+++ dot_config/nvim/lua/user/lualine.lua
@@ -0,0 +1,67 @@
+-- Setup lualine.
+-- see https://github.com/nvim-lualine/lualine.nvim
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+  vim.notify("could not find lualine plugin")
+  return
+end
+
+lualine.setup({
+  options = {
+    icons_enabled = true,
+    theme = 'ayu',  -- automatically load the theme matching g:ayucolor
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {'NvimTree', 'toggleterm'},
+    },
+    always_divide_middle = true,
+    globalstatus = true,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff'},
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+        shorting_target = 40,
+      },
+    },
+    lualine_w = {
+      'diagnostics',
+    },
+    lualine_x = {
+      'diagnostics',
+      {
+        'filetype',
+        icon_only = true,
+        icon = { align = 'right' },
+      },
+    },
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {
+    lualine_c = {'filename'},
+  },
+  inactive_winbar = {
+    lualine_c = {'filename'},
+    },
+  extensions = {'fzf', 'fugitive', 'nvim-tree', 'quickfix', 'toggleterm'}
+})
