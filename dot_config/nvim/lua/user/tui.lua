e124c049b3e754068078b0c54c176a5fc3771e53
Guilhem C <guilhem.charles@gmail.com>
2022-10-09 21:38:24 +0200

    nvim: move colorscheme, lualine, and nvim-tree initialization into tui module

--- dot_config/nvim/lua/user/tui.lua
+++ dot_config/nvim/lua/user/tui.lua
@@ -1,4 +1,16 @@
+require "user.lualine"
+require "user.nvim-tree"
+
 -- True Color (24-bit) configuration for alacritty + tmux
 -- kudos to https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
 vim.o.termguicolors = true
 
+-- Setup colorscheme.
+-- see https://github.com/Shatur/neovim-ayu
+local status_ok, _ = pcall(require, "ayu")
+if not status_ok then
+  vim.notify("could not find ayu plugin")
+else
+  -- Note: when calling ayu.colorscheme() NvimTree and Lualine do not pick-up the theme..
+  vim.cmd.colorscheme("ayu")
+end
