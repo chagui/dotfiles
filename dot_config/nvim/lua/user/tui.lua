3012f5aa31db778b2ff85f04af16bf5db4b23515
Guilhem C <guilhem.charles@gmail.com>
2022-10-09 17:27:49 +0200

    nvim: replace vim ayu colorscheme with its lua re-implementation

--- dot_config/nvim/lua/user/tui.lua
+++ dot_config/nvim/lua/user/tui.lua
@@ -2,10 +2,3 @@
 -- kudos to https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
 vim.o.termguicolors = true
 
-local success, _ = pcall(vim.cmd, [[
-    let ayucolor="dark"
-    colorscheme ayu
-]])
-if not success then
-    vim.notify("could not set ayu colorscheme!")
-end
