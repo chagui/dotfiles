4272ffdd64c2536e9749663426fc05442a3ae27d
Guilhem C <guilhem.charles@gmail.com>
2022-10-21 20:48:16 +0200

    nvim: refactor options to use a local table

--- dot_config/nvim/lua/user/options.lua
+++ dot_config/nvim/lua/user/options.lua
@@ -1,36 +1,40 @@
--- tip: use vim.inspect to know what's inside a variable, e.g. print(vim.inspect(opts.<option>))
-local opts = vim.opt
+-- tip: use vim.inspect to know what's inside a variable, e.g. print(vim.inspect(vim.opt.syntax))
+local options = {
+  syntax = "on",
 
-opts.syntax = "on"
+  ttyfast = true,
+  showmode = true,
+  showcmd = true,
 
-opts.ttyfast = true
-opts.showmode = true
-opts.showcmd = true
+  -- Tab sanity
+  expandtab = true,
+  shiftwidth = 4,
+  softtabstop = 4,
+  smartindent = true,
+  autoindent = true,
 
--- Tab sanity
-opts.expandtab = true
-opts.shiftwidth=4
-opts.softtabstop=4
-opts.smartindent = true
-opts.autoindent = true
+  -- Navigation
+  title = true,
+  number = true,
+  relativenumber = true,
+  mouse = "a",
+  cursorline = true,
+  -- Always use the clipboard for all operations instead of interacting with
+  -- registers
+  clipboard = "unnamedplus",
 
--- Navigation
-opts.title = true
-opts.number = true
-opts.relativenumber = true
-opts.mouse = "a"
-opts.cursorline = true
--- Always use the clipboard for all operations instead of interacting with
--- registers
-opts.clipboard = "unnamedplus"
+  -- Search
+  ignorecase = true,
+  smartcase = true,
+  hlsearch = true,
+  incsearch = true,
 
--- Search
-opts.ignorecase = true
-opts.smartcase = true
-opts.hlsearch = true
-opts.incsearch = true
+  -- History
+  undodir = vim.fn.stdpath "cache" .. "/undo",
+  undofile = true,
+}
 
--- History
-opts.undodir = vim.fn.stdpath "cache" .. "/undo"
-opts.undofile = true
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
 
