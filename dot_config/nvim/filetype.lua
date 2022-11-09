76cb6499fcbbe3ca4ae51883ebfd581971d55ae6
Guilhem C <guilhem.charles@gmail.com>
2022-11-09 19:15:06 +0100

    nvim: set tab preferences for hcl

--- dot_config/nvim/filetype.lua
+++ dot_config/nvim/filetype.lua
@@ -29,7 +29,7 @@ vim.filetype.add({
 local set = vim.opt
 -- https://neovim.io/doc/user/api.html#nvim_create_autocmd()
 vim.api.nvim_create_autocmd({"FileType"}, {
-    pattern = {"json", "yaml"},
+    pattern = {"hcl", "json", "yaml"},
     callback = function()
         set.tabstop = 2
         set.shiftwidth = 2
