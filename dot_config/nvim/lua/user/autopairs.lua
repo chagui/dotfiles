14142d2ba9c4098d65cff7c6b93bd9c8d43c8dbd
Guilhem C <guilhem.charles@gmail.com>
2022-12-21 19:19:56 +0100

    nvim: try to load autopairs completion only if cmp module is present

--- dot_config/nvim/lua/user/autopairs.lua
+++ dot_config/nvim/lua/user/autopairs.lua
@@ -27,12 +27,11 @@ npairs.setup {
   },
 }
 
-local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
   vim.notify("could not find cmp plugin")
   return
 end
+local cmp_autopairs = require "nvim-autopairs.completion.cmp"
 
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
