1dce5889a8bd5f380fe063928c7a13e5bbe65005
Guilhem C <guilhem.charles@gmail.com>
2022-12-23 17:49:30 +0100

    nvim: move nvim-tree configuration to the after/plugin directory

--- dot_config/nvim/lua/user/tui.lua
+++ dot_config/nvim/lua/user/tui.lua
@@ -1,6 +1,5 @@
 require "user.bufferline"
 require "user.lualine"
-require "user.nvim-tree"
 
 -- True Color (24-bit) configuration for alacritty + tmux
 -- kudos to https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
