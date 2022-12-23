44b4a723fe57a95d80b9440743378ac34997008a
Guilhem C <guilhem.charles@gmail.com>
2022-12-23 18:01:20 +0100

    nvim: move lualine configuration to the after/plugin directory

--- dot_config/nvim/lua/user/tui.lua
+++ dot_config/nvim/lua/user/tui.lua
@@ -1,5 +1,3 @@
-require "user.lualine"
-
 -- True Color (24-bit) configuration for alacritty + tmux
 -- kudos to https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
 vim.o.termguicolors = true
