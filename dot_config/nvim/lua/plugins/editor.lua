004ea45bc866fcb399087bd75e86ee64cd724727
Guilhem C <guilhem.charles@gmail.com>
2023-03-14 22:03:03 +0100

    nvim: move gitsigns config to after/plugin and enable current line blame

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -4,9 +4,6 @@ return {
     {
         "lewis6991/gitsigns.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("gitsigns").setup()
-        end,
     },
     "justinmk/vim-sneak",
 }
