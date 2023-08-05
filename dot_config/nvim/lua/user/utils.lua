3344ff22d810b92abd8f041c212fd70a59a241a3
Guilhem C <guilhem.charles@gmail.com>
2023-08-05 11:41:50 +0200

    neovim: add opts arg to map functions and ensure defaults

--- dot_config/nvim/lua/user/utils.lua
+++ dot_config/nvim/lua/user/utils.lua
@@ -11,45 +11,64 @@ function utils.is_vscode()
     return vim.g.vscode
 end
 
--- todo: add opts arg and use table merge
-local function keymap(mode, lhs, rhs)
-    vim.api.nvim_set_keymap(mode, lhs, rhs, { silent = true })
+local function set_if_absent(lhs, rhs)
+    for k, v in pairs(rhs) do
+        if lhs[k] == nil then
+            lhs[k] = v
+        end
+    end
 end
 
-function utils.map(lhs, rhs)
-    keymap("", lhs, rhs)
-end
-function utils.nmap(lhs, rhs)
-    keymap("n", lhs, rhs)
-end
-function utils.imap(lhs, rhs)
-    keymap("i", lhs, rhs)
-end
-function utils.xmap(lhs, rhs)
-    keymap("x", lhs, rhs)
-end
-function utils.vmap(lhs, rhs)
-    keymap("v", lhs, rhs)
+local function keymap(mode, lhs, rhs, opts)
+    opts = opts and opts or {}
+    set_if_absent(opts, { silent = true })
+    vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
 end
 
-local function keynoremap(mode, lhs, rhs)
-    vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
+function utils.map(lhs, rhs, opts)
+    keymap("", lhs, rhs, opts)
 end
 
-function utils.noremap(lhs, rhs)
-    keynoremap("", lhs, rhs)
+function utils.nmap(lhs, rhs, opts)
+    keymap("n", lhs, rhs, opts)
 end
-function utils.nnoremap(lhs, rhs)
-    keynoremap("n", lhs, rhs)
+
+function utils.imap(lhs, rhs, opts)
+    keymap("i", lhs, rhs, opts)
 end
-function utils.inoremap(lhs, rhs)
-    keynoremap("i", lhs, rhs)
+
+function utils.xmap(lhs, rhs, opts)
+    keymap("x", lhs, rhs, opts)
 end
-function utils.xnoremap(lhs, rhs)
-    keynoremap("x", lhs, rhs)
+
+function utils.vmap(lhs, rhs, opts)
+    keymap("v", lhs, rhs, opts)
 end
-function utils.vnoremap(lhs, rhs)
-    keynoremap("v", lhs, rhs)
+
+local function keynoremap(mode, lhs, rhs, opts)
+    opts = opts and opts or {}
+    set_if_absent(opts, { noremap = true, silent = true })
+    vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+end
+
+function utils.noremap(lhs, rhs, opts)
+    keynoremap("", lhs, rhs, opts)
+end
+
+function utils.nnoremap(lhs, rhs, opts)
+    keynoremap("n", lhs, rhs, opts)
+end
+
+function utils.inoremap(lhs, rhs, opts)
+    keynoremap("i", lhs, rhs, opts)
+end
+
+function utils.xnoremap(lhs, rhs, opts)
+    keynoremap("x", lhs, rhs, opts)
+end
+
+function utils.vnoremap(lhs, rhs, opts)
+    keynoremap("v", lhs, rhs, opts)
 end
 
 return utils
