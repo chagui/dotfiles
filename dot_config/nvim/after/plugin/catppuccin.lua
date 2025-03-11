dfd9f7271e751dd8eda680f3f87126286eefd34c
Guilhem C. <guilhem.charles@gmail.com>
2025-03-11 23:19:40 +0100

    nvim: configure integrations for catppuccin

--- dot_config/nvim/after/plugin/catppuccin.lua
+++ dot_config/nvim/after/plugin/catppuccin.lua
@@ -1,4 +1,11 @@
 require("catppuccin").setup({
     flavor = "macchiato",
+    integrations = {
+        cmp = true,
+        fzf = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+    },
 })
 vim.cmd.colorscheme("catppuccin")
