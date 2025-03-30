c9047413aac4e449df84b8b0b617e779768b311e
Guilhem C. <guilhem.charles@gmail.com>
2025-03-30 22:08:26 +0200

    feat(nvim): moving the cursor after a search clears the highlights

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -44,3 +44,15 @@ else
         vim.o.grepformat = "f:%l:%c:%m"
     end
 end
+
+-- Moving the cursor after a search clears the highlights (same Ctrl+L)
+vim.api.nvim_create_autocmd("CursorMoved", {
+    group = vim.api.nvim_create_augroup("auto-hlsearch", { clear = true }),
+    callback = function()
+        if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+            vim.schedule(function()
+                vim.cmd.nohlsearch()
+            end)
+        end
+    end,
+})
