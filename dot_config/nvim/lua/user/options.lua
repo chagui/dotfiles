9be2d10babf5c638456c42a07283798f934b8290
Guilhem C. <guilhem.charles@gmail.com>
2025-03-30 20:21:40 +0200

    feat(nvim): extract options relative to terminal usage to tui package

--- dot_config/nvim/lua/user/options.lua
+++ dot_config/nvim/lua/user/options.lua
@@ -3,23 +3,6 @@ local utils = require("user.utils")
 utils.set_options({
     syntax = "on",
 
-    ttyfast = true,
-    showmode = true,
-    showcmd = true,
-
-    -- Tab sanity
-    expandtab = true,
-    shiftwidth = 4,
-    softtabstop = 4,
-    smartindent = true,
-    autoindent = true,
-
-    -- Navigation
-    title = true,
-    number = true,
-    relativenumber = true,
-    mouse = "a",
-    cursorline = true,
     -- Always use the clipboard for all operations instead of interacting with
     -- registers
     clipboard = "unnamedplus",
@@ -29,8 +12,4 @@ utils.set_options({
     smartcase = true,
     hlsearch = true,
     incsearch = true,
-
-    -- History
-    undodir = vim.fn.stdpath("cache") .. "/undo",
-    undofile = true,
 })
