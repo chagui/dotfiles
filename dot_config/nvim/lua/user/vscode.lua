f4dcce9dcead4a633d1e5472f1e502737a35aa16
Guilhem C. <guilhem.charles@gmail.com>
2025-03-30 22:11:26 +0200

    feat(nvim): keyboard shortcuts for folding, unfolding and focus the file explorer panel

--- dot_config/nvim/lua/user/vscode.lua
+++ dot_config/nvim/lua/user/vscode.lua
@@ -12,3 +12,15 @@ utils.nmap("<leader>s", "<Cmd>w<CR>")
 
 -- preserve the unnamed register (changed to clipboard above) by default
 utils.vmap("p", "P")
+
+local vscode_action = function(action)
+    return function()
+        require("vscode").action(action)
+    end
+end
+
+utils.nmap("<leader>[", vscode_action("editor.fold"))
+utils.nmap("<leader>]", vscode_action("editor.unfold"))
+
+-- Focus the file explorer panel
+utils.nmap("<leader>e", vscode_action("workbench.explorer.fileView.focus"))
