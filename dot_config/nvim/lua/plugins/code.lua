fb48732039b2d47f97aa6751f9573e44557a152c
Guilhem C. <guilhem.charles@gmail.com>
2025-04-02 22:54:36 +0200

    refactor(nvim): move autopairs config from after to lazy

--- dot_config/nvim/lua/plugins/code.lua
+++ dot_config/nvim/lua/plugins/code.lua
@@ -1,5 +1,28 @@
 return {
-    { "windwp/nvim-autopairs", event = "InsertEnter" },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {
+            check_ts = true,
+            ts_config = {
+                lua = { "string", "source" },
+                javascript = { "string", "template_string" },
+                java = false,
+            },
+            disable_filetype = { "TelescopePrompt", "spectre_panel" },
+            fast_wrap = {
+                map = "<M-e>",
+                chars = { "{", "[", "(", '"', "'" },
+                pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+                offset = 0, -- Offset from pattern match
+                end_key = "$",
+                keys = "qwertyuiopzxcvbnmasdfghjkl",
+                check_comma = true,
+                highlight = "PmenuSel",
+                highlight_grey = "LineNr",
+            },
+        },
+    },
     "folke/neodev.nvim",
 
     -- Treesitter
