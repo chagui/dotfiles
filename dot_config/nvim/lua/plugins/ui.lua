4d64ee8e81dd475d519f738e05a9f922098a4be9
Guilhem C. <guilhem.charles@gmail.com>
2025-04-04 22:05:14 +0200

    refactor(nvim): move telescope config from after to lazy

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -156,9 +156,146 @@ return {
     },
     {
         "nvim-telescope/telescope.nvim",
+        cmd = "Telescope",
+        version = false, -- telescope did only one release, so use HEAD for now
         dependencies = {
             { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         },
+        keys = {
+            {
+                "<leader>,",
+                "<cmd>Telescope buffers sort_mru=true sort_lastused=true ignore_current_buffer=true<cr>",
+                desc = "Show Buffers by most recently used",
+            },
+            { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep (Root Dir)" },
+            { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+            { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (Root Dir)" },
+            { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "[S]earch [C]ommands" },
+            { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "[S]earch [D]iagnostics" },
+            { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "[S]earch Man [P]ages" },
+            { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "[S]earch [B]uffer" },
+            { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "[S]earch Document [D]iagnostics" },
+            { "<leader>sq", "<cmd>Telescope quickfix<cr>", desc = "[S]earch [Q]uickfix List" },
+            { "<leader>sr", "<cmd>Telescope resume<cr>", desc = "[S]earch [R]esume" },
+            { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "[S]earch [H]elp Tags" },
+            { "<leader>sw", "<cmd>Telescope grep_string<cr>", desc = "[S]earch Current [W]ord" },
+            {
+                "<leader>fb",
+                "<cmd>Telescope buffers sort_mru=true sort_lastused=true ignore_current_buffer=true<cr>",
+                desc = "[F]ind [B]uffers",
+            },
+            -- git
+            { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Search Vim [O]ptions" },
+            { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "Search [G]it [C]ommits" },
+            { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Search [G]it [S]tatus" },
+            { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "[F]ind [F]iles (git-files)" },
+            -- LSP
+            { "<leader>ss", "<cmd>Telescope lsp_document_symbols<cr>", desc = "[S]earch [S]ymbol" },
+            { "<leader>sS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "[S]earch (Workspace) Symbol" },
+        },
+        opts = function()
+            local telescope = require("telescope")
+
+            local actions = require("telescope.actions")
+
+            local opts = {
+                defaults = {
+                    vimgrep_arguments = {
+                        "rg",
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                        "--hidden",
+                    },
+                    prompt_prefix = " ",
+                    selection_caret = " ",
+                    path_display = { "smart" },
+
+                    mappings = {
+                        i = {
+                            ["<C-n>"] = actions.cycle_history_next,
+                            ["<C-p>"] = actions.cycle_history_prev,
+
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-k>"] = actions.move_selection_previous,
+
+                            ["<C-c>"] = actions.close,
+
+                            ["<Down>"] = actions.move_selection_next,
+                            ["<Up>"] = actions.move_selection_previous,
+
+                            ["<CR>"] = actions.select_default,
+                            ["<C-x>"] = actions.select_horizontal,
+                            ["<C-v>"] = actions.select_vertical,
+                            ["<C-t>"] = actions.select_tab,
+
+                            ["<C-u>"] = actions.preview_scrolling_up,
+                            ["<C-d>"] = actions.preview_scrolling_down,
+
+                            ["<PageUp>"] = actions.results_scrolling_up,
+                            ["<PageDown>"] = actions.results_scrolling_down,
+
+                            ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+                            ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+                            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+                            ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                            ["<C-l>"] = actions.complete_tag,
+                            ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+                        },
+
+                        n = {
+                            ["<esc>"] = actions.close,
+                            ["<CR>"] = actions.select_default,
+                            ["<C-x>"] = actions.select_horizontal,
+                            ["<C-v>"] = actions.select_vertical,
+                            ["<C-t>"] = actions.select_tab,
+
+                            ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+                            ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+                            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+                            ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+
+                            ["j"] = actions.move_selection_next,
+                            ["k"] = actions.move_selection_previous,
+                            ["H"] = actions.move_to_top,
+                            ["M"] = actions.move_to_middle,
+                            ["L"] = actions.move_to_bottom,
+
+                            ["<Down>"] = actions.move_selection_next,
+                            ["<Up>"] = actions.move_selection_previous,
+                            ["gg"] = actions.move_to_top,
+                            ["G"] = actions.move_to_bottom,
+
+                            ["<C-u>"] = actions.preview_scrolling_up,
+                            ["<C-d>"] = actions.preview_scrolling_down,
+
+                            ["<PageUp>"] = actions.results_scrolling_up,
+                            ["<PageDown>"] = actions.results_scrolling_down,
+
+                            ["?"] = actions.which_key,
+                        },
+                    },
+                },
+            }
+
+            if vim.loop.os_uname().sysname == "linux" then
+                telescope.load_extension("media_files")
+                opts.extensions.media_files = {
+                    filetypes = { "png", "webp", "jpg", "jpeg" },
+                    find_cmd = "rg",
+                }
+            end
+
+            local has_native_fzf, _ = pcall(require, "fzf_lib")
+            if has_native_fzf then
+                telescope.load_extension("fzf")
+            end
+
+            return opts
+        end,
     },
     {
         "nvim-telescope/telescope-frecency.nvim",
