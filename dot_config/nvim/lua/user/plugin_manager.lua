c64b4d55ae5302bc537f36b33d70d702521228ca
Guilhem C. <guilhem.charles@gmail.com>
2025-09-27 18:06:38 +0200

    nvim: modernize lazy set-up

--- dot_config/nvim/lua/user/plugin_manager.lua
+++ dot_config/nvim/lua/user/plugin_manager.lua
@@ -1,7 +1,7 @@
 -- Install lazy.nvim if not found
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
+if not vim.uv.fs_stat(lazypath) then
+    local out = vim.fn.system({
         "git",
         "clone",
         "--filter=blob:none",
@@ -9,6 +9,15 @@ if not vim.loop.fs_stat(lazypath) then
         "--branch=stable", -- latest stable release
         lazypath,
     })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
