0de8908e5d2d555baa9fc0251a9c810209056f86
Guilhem C. <guilhem.charles@gmail.com>
2025-09-27 18:06:20 +0200

    nvim: migrate from neodev to lazydev

--- dot_config/nvim/lua/plugins/linux/telescope-media-files.lua
+++ dot_config/nvim/lua/plugins/linux/telescope-media-files.lua
@@ -1,3 +1,4 @@
+---@type LazySpec
 return {
     {
         "nvim-telescope/telescope-media-files.nvim",
--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,3 +1,4 @@
+---@type LazySpec
 return {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     {
