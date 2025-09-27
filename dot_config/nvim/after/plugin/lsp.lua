1373f40b7679e8b95601753ac40c43bf0af548a7
Guilhem C. <guilhem.charles@gmail.com>
2025-09-27 15:03:09 +0200

    todo(nvim): migrate from neodev to lazydev

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,3 +1,4 @@
+-- todo: migrate to https://github.com/folke/lazydev.nvim
 -- IMPORTANT: make sure to setup neodev BEFORE lspconfig
 -- see: https://github.com/folke/neodev.nvim/blob/8fd21037453f4306f500e437c5cbdf6e8b6c2f99/README.md#-setup
 local status_ok, neodev = pcall(require, "neodev")
