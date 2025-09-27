63f320dc26ce0f8dfa09990ec6c0cbe52744fdd8
Guilhem C. <guilhem.charles@gmail.com>
2025-09-27 23:07:06 +0200

    nvim: migrate to blink completion

--- dot_config/nvim/lua/plugins/code.lua
+++ dot_config/nvim/lua/plugins/code.lua
@@ -51,7 +51,118 @@ return {
                     { "<leader>mm", "<Cmd>Mason<CR>", desc = "Packages" },
                 },
             },
-            "neovim/nvim-lspconfig",
+            {
+                "neovim/nvim-lspconfig",
+                dependencies = { "saghen/blink.cmp" },
+            },
         },
+    },
+
+    -- Completion
+    {
+        "saghen/blink.cmp",
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+            "kyazdani42/nvim-web-devicons",
+            "onsails/lspkind.nvim",
+        },
+        version = "1.*",
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            keymap = {
+                preset = "enter",
+                ["<C-S>"] = { "show", "show_documentation", "hide_documentation" },
+                ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+                ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+            },
+            completion = {
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 500,
+                },
+                ghost_text = {
+                    enabled = true,
+                },
+                list = {
+                    selection = {
+                        preselect = true,
+                        -- use ghost text instead
+                        auto_insert = false,
+                    },
+                },
+                -- based on of https://cmp.saghen.dev/recipes.html#nvim-web-devicons-lspkind
+                menu = {
+                    draw = {
+                        columns = {
+                            {
+                                "label",
+                                gap = 1,
+                                "label_description",
+                            },
+                            {
+                                "kind_icon",
+                                gap = 1,
+                                "kind",
+                            },
+                            {
+                                "source_name",
+                            },
+                        },
+                        components = {
+                            kind_icon = {
+                                text = function(ctx)
+                                    local icon = ctx.kind_icon
+                                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                                        local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                                        if dev_icon then
+                                            icon = dev_icon
+                                        end
+                                    else
+                                        icon = require("lspkind").symbolic(ctx.kind, {
+                                            mode = "symbol",
+                                        })
+                                    end
+
+                                    return icon .. ctx.icon_gap
+                                end,
+
+                                -- Optionally, use the highlight groups from nvim-web-devicons
+                                -- You can also add the same function for `kind.highlight` if you want to
+                                -- keep the highlight groups in sync with the icons.
+                                highlight = function(ctx)
+                                    local hl = ctx.kind_hl
+                                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                                        local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                                        if dev_icon then
+                                            hl = dev_hl
+                                        end
+                                    end
+                                    return hl
+                                end,
+                            },
+                        },
+                    },
+                },
+                trigger = {
+                    show_on_keyword = true,
+                    show_on_trigger_character = true,
+                    show_on_insert_on_trigger_character = true,
+                },
+            },
+            sources = {
+                default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+                providers = {
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+                        -- make lazydev completions top priority (see `:h blink.cmp`)
+                        score_offset = 100,
+                    },
+                },
+            },
+            signature = { enabled = true },
+        },
+        opts_extend = { "sources.default" },
     },
 }
