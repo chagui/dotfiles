462fffbc8722feeed46f52775ab8ffd05f54cda9
Guilhem C. <guilhem.charles@gmail.com>
2026-02-19 11:10:15 +0100

    chore(zsh): drop pyenv

--- .chezmoiscripts/run_once_after_10_install_python_toolchains.sh
+++ .chezmoiscripts/run_once_after_10_install_python_toolchains.sh
@@ -5,9 +5,9 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 # install the latest Python version
 uv python install
 
-: "${XDG_DATA_HOME:=$HOME/.local/state}"
+: "${XDG_DATA_HOME:=$HOME/.local/share}"
 uv generate-shell-completion zsh >"${XDG_DATA_HOME}/zsh/completions/_uv"
 uvx --generate-shell-completion zsh >"$XDG_DATA_HOME/zsh/completions/_uvx"
 
-pip install argcomplete
+uv pip install argcomplete
 activate-global-python-argcomplete
